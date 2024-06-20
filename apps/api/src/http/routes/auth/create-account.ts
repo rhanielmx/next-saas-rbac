555a890b4bd547351bbba9092ed05cf8f514f14d
rhanielmx <rhanielmag.xavier@gmail.com>
2024-06-20 10:35:19 -0300

    Structure: Deleting unused files and creating initial structure

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,48 @@
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { hash } from 'bcryptjs'
+
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      if(userWithSameEmail) {
+        return reply.status(400).send({message: 'We already have an user registered with this email.'})
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        }
+      })
+
+      return reply.status(201).send()
+
+    },
+  )
+}
